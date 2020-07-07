0a40bc121c13848ace071aeb41b4e7a0b867d4d3
Patrickposales <patrickposales@gmail.com>
2020-07-07 02:01:59 -0400

    generadores parte 2 y excepciones

---
+++ Excepciones2-1.py
@@ -0,0 +1,11 @@
+def divide():
+
+	op1=(float(input("Introduce el primer numero: ")))
+
+	op2=(float(input("Introduce el primer numero: ")))
+
+	print("La division es:" + str(op1/op2))
+
+	print("Calculo finalizado")
+
+divide()
